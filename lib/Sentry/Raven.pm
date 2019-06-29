46ea9423c98cfd6b598c9121924353367b45b0fe
Matt Harrington <querry43@gmail.com>
2019-06-29 12:06:53 -0700

    changing perl versions to test against, incrementing version number

--- lib/Sentry/Raven.pm
+++ lib/Sentry/Raven.pm
@@ -6,7 +6,7 @@ use warnings;
 use Moo;
 use MooX::Types::MooseLike::Base qw/ ArrayRef HashRef Int Str /;
 
-our $VERSION = '1.10';
+our $VERSION = '1.11';
 
 use Data::Dump 'dump';
 use Devel::StackTrace;
@@ -59,7 +59,7 @@ Sentry::Raven - A perl sentry client
 
 =head1 VERSION
 
-Version 1.10
+Version 1.11
 
 =head1 SYNOPSIS
 
